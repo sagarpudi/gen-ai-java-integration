f902d3a844d3b6b0f3b9b878f433871332c0c689
Sagar Pudi <sagar_pudi@epam.com>
2025-04-07 23:26:46 +0530

    TASK 3: dynamically passing deployment models to play and compare

--- src/main/java/com/epam/training/gen/ai/chat/util/ValidationUtils.java
+++ src/main/java/com/epam/training/gen/ai/chat/util/ValidationUtils.java
@@ -15,4 +15,16 @@ public class ValidationUtils {
             throw new IllegalArgumentException(INVALID_TEMPERATURE_MESSAGE);
         }
     }
+
+    public static void validateDeploymentModel(DeploymentModel deploymentModel) {
+        if (deploymentModel == null || deploymentModel.getModelName() == null || deploymentModel.getModelName().trim().isEmpty()) {
+            throw new IllegalArgumentException("Deployment model name cannot be  null or empty");
+        }
+        if (DeploymentModel.valuesAsStream()
+                .noneMatch(model -> model.getModelName().equals(deploymentModel.getModelName()))) {
+            throw new IllegalArgumentException("Deployment model name is not valid");
+        }
+       
+        
+    }
 }
